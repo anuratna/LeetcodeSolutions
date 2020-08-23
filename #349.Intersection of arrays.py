7d24f1d10a06fa98b459e011b5ca47044255126e
anuratna <anuratna28@gmail.com>
2020-08-23 15:51:34 -0400

    Update #349.Intersection of arrays.py

--- #349.Intersection of arrays.py
+++ #349.Intersection of arrays.py
@@ -1,9 +1,17 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        result = []
-        for each in nums1:
-            if(each in nums2) & ( each not in result): 
-                # if each not in result:
-                result.append(each)
+        result =[]
+        set1 = set(nums1)
+        set2=set(nums2)
+        
+        if(len(set1)<len(set2)):
+            for each in set1:
+                if each in set2:
+                     result.append(each)
+            return result
+        else:
+            for each in set2:
+                if each in set1:
+                    result.append(each)
+            return result
                 
-        return result      
